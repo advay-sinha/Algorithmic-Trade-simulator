7138ac89c0d6bdab25ee80fc9464149dad8a13cc
advay-sinha <advaysinhaa@gmail.com>
2025-02-12 03:56:30 +0530

    first commit

---
+++ backend/src/models/User.java
@@ -0,0 +1,9 @@
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;  // Store hashed password
+    private String otp;
+    private boolean isVerified;
+}
